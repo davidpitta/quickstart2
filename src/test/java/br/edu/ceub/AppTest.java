5a5a38635e3d6f86479d18e724dcc2f60ec0d965
david.pitta <davidfrpitta@gmail.com>
2025-03-06 09:25:01 -0300

    Adicionando método de somar e testes do método

--- src/test/java/br/edu/ceub/AppTest.java
+++ src/test/java/br/edu/ceub/AppTest.java
@@ -1,5 +1,6 @@
 package br.edu.ceub;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,41 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    void somar_2_e_3_retorna_5() {
+        // Preparar
+        App appUnderTest = new App();
+
+        // Executar
+        int  result = appUnderTest.somar(2, 3);
+
+        // Verificar
+        assertEquals(5, result);
+    }
+
+    @Test
+    void soma_numero_negativo_com_positivo() {
+        // Preparar
+        App appUnderTest = new App();
+
+        // Executar
+        int  result = appUnderTest.somar(5, -3);
+
+        // Verificar
+        assertEquals(2, result);
+    }
+
+    @Test
+    void soma_envolvendo_numero_0() {
+        // Preparar
+        App appUnderTest = new App();
+
+        // Executar
+        int  result = appUnderTest.somar(3, 0);
+
+        // Verificar
+        assertEquals(3, result);
     }
 }
